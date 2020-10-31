d352602b817b9b375975c1efe095fb453aa18d9f
snirco <cohensnir@gmail.com>
2020-10-31 20:22:43 +0200

    removed

--- src/components/Auctions/AuctionCard/AuctionCard.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./AuctionCard.scss";
-
-const AuctionCard = ({ children, className = "" }: any) => {
-  return <div className={`${className} auctionCard`}>{children}</div>;
-};
-
-export default AuctionCard;
--- src/components/Auctions/AuctionCard/AuctionCardImage.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const AuctionCardImage = ({ url, className }: any) => {
-  return <img src={url} alt="wait for it" className={className} />;
-};
-
-export default AuctionCardImage;
